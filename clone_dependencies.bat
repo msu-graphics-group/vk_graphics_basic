0f0171ec5e89c7fb72c061d0caaaf9bc55674b9c
vsan-win <vadim.sanzharov@graphics.cs.msu.ru>
2021-08-25 18:08:57 +0300

    windows additions and fixes

---
+++ clone_dependencies.bat
@@ -0,0 +1,3 @@
+cd external
+git clone --depth 1 https://github.com/zeux/volk.git
+git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
--- clone_deps_and_build.bat
+++
@@ -1,8 +0,0 @@
-cd external
-git clone --depth 1 https://github.com/zeux/volk.git
-git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
-cd ..
-mkdir build
-cd build
-cmake ..
-make -j8
