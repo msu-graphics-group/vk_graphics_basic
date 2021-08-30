0cf28154d3c31cabcd44fc0830df39c32a830f17
Vladimir Frolov <vova@frolov.pp.ru>
2021-08-30 13:04:25 +0300

    add VS Code configs

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
   std::shared_ptr<IRender> app = CreateRender(1024, 1024, RenderEngineType::SIMPLE_FORWARD);
-  auto* window = Init(app, 1);
+  auto* window = Init(app, 0);
 
   app->LoadScene("../resources/scenes/043_cornell_normals/statex_00001.xml", false);
 
