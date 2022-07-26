78b0f0b6351f564eab096db99f3338198519549e
AlexandrShcherbakov <alex.shcherbakov.as@gmail.com>
2022-07-26 08:26:55 +0300

    Add etna repository to dependencies

--- clone_dependencies.bat
+++ clone_dependencies.bat
@@ -2,3 +2,4 @@ cd external
 git clone --depth 1 https://github.com/zeux/volk.git
 git clone --depth 1 https://github.com/ocornut/imgui.git
 git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
+git clone --depth 1 https://github.com/AlexandrShcherbakov/etna.git
