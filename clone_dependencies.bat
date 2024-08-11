2aa166fffa555034ab82f9949fd75e77a940c1da
Roman Sandu <roma.sandu@gmail.com>
2024-08-11 20:04:35 +0300

    Delete some files
    Delete old model loading code and some IDE-specific stuff that I accidentally committed.

--- clone_dependencies.bat
+++
@@ -1,5 +0,0 @@
-cd external
-git clone --depth 1 https://github.com/ocornut/imgui.git
-git clone --depth 1 https://github.com/msu-graphics-group/vk-utils.git vkutils
-git clone --depth 1 https://github.com/AlexandrShcherbakov/etna.git
-git clone --depth 1 https://github.com/g-truc/glm.git
--- src/loader_utils/images.h
+++
@@ -1,8 +0,0 @@
-#ifndef VK_GRAPHICS_BASIC_IMAGES_H
-#define VK_GRAPHICS_BASIC_IMAGES_H
-
-unsigned char* loadImageLDR(const char* a_filename, int &w, int &h, int &channels);
-
-void freeImageMemLDR(unsigned char* pixels);
-
-#endif// VK_GRAPHICS_BASIC_IMAGES_H
